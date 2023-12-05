aeee0833be39d6ceaf7d6ec2f679903e94ed6acf
Anton Khlebnikov <toni.farsh@icloud.com>
2023-12-05 20:34:53 +0500

    instructionApp

---
+++ index.js
@@ -0,0 +1,27 @@
+const info = document.querySelector('.info');
+const instrs = document.querySelectorAll('.instr');
+
+function goInstuction() {
+    instrs.forEach(e => e.classList.add('hidden'));
+    info.classList.add('hidden');
+    document.querySelector('.screen').style.backgroundColor = "#9B9B9B"
+    
+
+    instrs[0].classList.remove('hidden');
+    instrs.forEach( instr => {
+        if(instr != instrs[4]) {
+            instr.addEventListener('click', () => {
+                instr.classList.add('hidden');
+                instr.nextElementSibling.classList.remove('hidden');
+            })
+        } else {
+            info.classList.remove('hidden');
+            document.querySelector('.close').addEventListener('click', ()=> {
+                instrs.forEach(e => e.classList.add('hidden'));
+                document.querySelector('.screen').style.backgroundColor = ""
+            })
+        }
+       
+})
+}
+
